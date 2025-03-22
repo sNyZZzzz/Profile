5f50252e95257e0a7cb16898a4877fc93fd06ca2
sNyZZzzz <christiaanmyburgh01@gmail.com>
2025-03-22 14:02:41 +0200

    clean up dependencies

--- app/projects/layout.tsx
+++ app/projects/layout.tsx
@@ -1,18 +1,16 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "@/app/_styles/globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
+import type { Metadata } from "next";
 
-const geistSans = localFont({
-  src: "../fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "../fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const geistSans = localFont({
+//   src: "../fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "../fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
 export const metadata: Metadata = {
   title: "Projects",
